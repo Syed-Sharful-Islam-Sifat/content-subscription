0ab1ece22a7e3a32db5070d84a70db188cfdc4c9
sifat <sharful.swe@gmail.com>
2025-02-23 21:39:11 +0600

    change port to 8080

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const userRoutes = require("./routes/userRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const feedRoutes = require("./routes/feedRoutes");
